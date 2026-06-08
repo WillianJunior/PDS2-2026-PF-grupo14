ab5887ade659f7b546be6d52aa9f8e5abf56969a
Kaio Gomes Vieira <06.kaio@gmail.com>
2026-06-07 23:49:28 -0300

    Modificacoes em Combate.cpp e Habilidade.cpp

--- src/Habilidade.cpp
+++ src/Habilidade.cpp
@@ -4,25 +4,38 @@
 // CONSTRUTOR
 // ============================================================================
 
-Habilidade::Habilidade(std::string nome, TipoHabilidade tipo, int custoE, int custoM, int valorBase, int duracao) {
-    _nome = nome; _tipo = tipo; _custoEnergia = custoE; _custoMP = custoM; 
-    _valorBase = valorBase; _duracao = duracao;
+Habilidade::Habilidade(std::string nome, TipoHabilidade tipo, int custoE, int custoM, int valorBase, int duracao)
+    : _nome(nome), _tipo(tipo), _custoEnergia(custoE), _custoMP(custoM), _valorBase(valorBase), _duracao(duracao) {
 }
 
 // ============================================================================
-// GETTERS E MÉTODOS DE CHECAGEM (Estilo Esqueleto/TDD Red Compacto)
+// GETTERS E MÉTODOS DE CHECAGEM
 // ============================================================================
 
-std::string Habilidade::getNome() const { return _nome; }
+std::string Habilidade::getNome() const { 
+    return _nome; 
+}
 
-TipoHabilidade Habilidade::getTipo() const { return _tipo; }
+TipoHabilidade Habilidade::getTipo() const { 
+    return _tipo; 
+}
 
-int Habilidade::getCustoEnergia() const { return _custoEnergia; }
+int Habilidade::getCustoEnergia() const { 
+    return _custoEnergia; 
+}
 
-int Habilidade::getCustoMP() const { return _custoMP; }
+int Habilidade::getCustoMP() const { 
+    return _custoMP; 
+}
 
-int Habilidade::getValorBase() const { return _valorBase; }
+int Habilidade::getValorBase() const { 
+    return _valorBase; 
+}
 
-int Habilidade::getDuracao() const { return _duracao; }
+int Habilidade::getDuracao() const { 
+    return _duracao; 
+}
 
-bool Habilidade::ehEfeitoContinuo() const { return _duracao > 0; }
+bool Habilidade::ehEfeitoContinuo() const { 
+    return _duracao > 0; 
+}
